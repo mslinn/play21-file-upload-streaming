ad3c1da460e297b637d86c2e41b811d4368a4dd3
Mike Slinn <mslinn@micronauticsresearch.com>
2013-05-16 06:52:02 -0700

    Updated to Scala 2.10.1, sbt 0.12.3 and current AWS S3 SDK for Java

--- project/Build.scala
+++ project/Build.scala
@@ -6,8 +6,10 @@ object ApplicationBuild extends Build {
   val appName         = "play21-file-upload-streaming"
   val appVersion      = "1.0-SNAPSHOT"
 
+  scalaVersion := "2.10.1"
+
   val appDependencies = Seq(
-    "com.amazonaws" % "aws-java-sdk" % "1.3.32"  withSources()
+    "com.amazonaws" % "aws-java-sdk" % "1.4.2.1"  withSources()
   )
 
   val main = play.Project(appName, appVersion, appDependencies)
