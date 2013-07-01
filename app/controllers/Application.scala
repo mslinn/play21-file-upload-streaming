d097a3db09e87b7ea10787d7aa40efebcd67561e
Mashhood Rastgar <mashhoodr@gmail.com>
2013-07-01 13:58:56 -0700

    created a custom output stream handler which communicates with the file service to upload s3 (in parts)

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,20 +4,23 @@ import play.api._
 import mvc._
 import services.StreamingBodyParser.streamingBodyParser
 import java.io.{FileOutputStream, File}
+import models.MultipartUploadHandler
 
 object Application extends Controller {
-  val welcomeMsg = "Demonstration of Streaming File Upload for Play 2.1"
+  val welcomeMsg = "Demonstration of Streaming File Upload for Play 2.1 with AWS S3"
 
   def index = Action {
     Ok(views.html.index(welcomeMsg))
   }
 
   /** Higher-order function that accepts the unqualified name of the file to stream to and returns the output stream
-    * for the new file. This example streams to a file, but streaming to AWS S3 is also possible */
+    * for the new file. This example streams to a file, but streaming to AWS S3 is also possible
+    *
+    * To store in more specific folder in S3, just add the path when passing to the function eg "test/path" + filename
+    *
+    * */
   def streamConstructor(filename: String) = {
-    val dir = new File(sys.env("HOME"), "uploadedFiles")
-    dir.mkdirs()
-    Option(new FileOutputStream(new File(dir, filename)))
+    Option(new MultipartUploadHandler(filename))
   }
 
   def upload = Action(streamingBodyParser(streamConstructor)) { request =>
