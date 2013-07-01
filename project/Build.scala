32fee62af0715f3f7513ea7c067842a8fc6c1e31
Mashhood Rastgar <mashhoodr@gmail.com>
2013-07-01 13:56:57 -0700

    configured dependencies for the fileService and S3 library

--- project/Build.scala
+++ project/Build.scala
@@ -1,5 +1,5 @@
 import sbt._
-import Keys._
+import sbt.Keys._
 import play.Project._
 
 object ApplicationBuild extends Build {
@@ -7,7 +7,11 @@ object ApplicationBuild extends Build {
   val appName         = "play21-file-upload-streaming"
   val appVersion      = "1.0-SNAPSHOT"
 
-  val appDependencies = Seq()
+  val appDependencies = Seq(
+    "org.scalaz" %% "scalaz-core" % "7.0.0",
+    "nl.rhinofly" %% "api-s3" % "1.7.2",
+    "com.github.nscala-time" %% "nscala-time" % "0.4.0"
+  )
 
   scalaVersion := "2.10.1"
 
@@ -15,7 +19,9 @@ object ApplicationBuild extends Build {
     "-Ywarn-adapted-args", "-Ywarn-value-discard", "-Xlint")
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here
+    resolvers += Resolver.url("Markus Jura github repo play-libraries", url("http://markusjura.github.com/play-libraries"))(Resolver.ivyStylePatterns),
+    resolvers += "Rhinofly Internal Repository" at "http://maven-repository.rhinofly.net:8081/artifactory/libs-release-local"
   )
 
 }
